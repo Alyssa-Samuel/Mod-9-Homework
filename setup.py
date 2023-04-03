229da8e7011ec32e2486d066b431dfad4794ae10
Alyssa Samuel <83474895+Alyssa-Samuel@users.noreply.github.com>
2023-04-02 22:54:13 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,5 @@
+from Booklover.booklover import booklover
+
+BookLoverTest = Booklover("Alyssa", ays8ms@virginia.edu, "fiction")
+BookLoverTest.add_book("The Great Gatsby", 4)
+BookLoverTest.num_books_read()
